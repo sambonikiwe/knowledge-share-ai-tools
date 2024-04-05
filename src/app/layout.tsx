9f616863a770157789173d087ed0f9d2140ca989
Mpilo Ntombela <72506370+MpiloNtombela@users.noreply.github.com>
2024-04-05 12:29:47 +0200

    Added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Navbar } from '@/components/navbar';
+import Footer from '@/components/footer';
 
 export const metadata: Metadata = {
   title: 'Rent a car',
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         <main className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8 py-8 md:py-12'>{children}</main>
+        <Footer />
       </body>
     </html>
   );
